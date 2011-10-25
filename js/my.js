02db160dc0650d8f47e16a0cfe8aadc4c1930c73
Leo Balter <leonardo.balter@gmail.com>
2011-10-25 12:13:02 -0200

    manifest appcache updated, yepnope to call share buttons

--- js/my.js
+++ js/my.js
@@ -62,6 +62,37 @@ thunderReady(function() {
 
 });
 
+yepnope({
+  load: ['https://apis.google.com/js/plusone.js', '//platform.twitter.com/widgets.js'],
+  complete: function() {
+    var shareElems = document.getElementsByClassName('share');
+    var i = 0, l = shareElems.length;
+    for ( ; i < l; i++) {
+      shareElems[i].style.display = 'block';
+    }
+    console.log('complete yep');
+  }
+});
+
+// Check if a new cache is available on page load.
+if (window.addEventListener && typeof window.applicationCache !== 'undefined') {
+  window.addEventListener('load', function(e) {
+
+    window.applicationCache.addEventListener('updateready', function(e) {
+      if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+        // Browser downloaded a new app cache.
+        // Swap it in and reload the page to get the new hotness.
+        window.applicationCache.swapCache();
+        if (confirm('A new version of this site is available. Load it?')) {
+          window.location.reload();
+        }
+      } else {
+        // Manifest didn't changed. Nothing new to server.
+      }
+    }, false);
+
+  }, false);
+}
 
 // Google Analytics
 var _gaq = _gaq || [];
